931dc9a5cbc5acfedd211301d6b7101f5e0ff86d
Kickmeplsss <168713028+Kickmeplsss@users.noreply.github.com>
2025-06-25 22:14:58 -0700

    Add files via upload

---
+++ styles/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:website/constants/colors.dart';
+
+BoxDecoration kHederDecoration = BoxDecoration(
+  gradient: const LinearGradient(
+    colors: [Colors.transparent, CustomColor.bgLight1],
+  ),
+  color: CustomColor.yellowPrimary,
+  borderRadius: BorderRadius.circular(100),
+);
